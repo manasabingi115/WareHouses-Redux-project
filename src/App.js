dedd1c17b78f9893e1709065f97140fe0cd1ebe8
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-02-21 23:54:25 -0800

    added search functionality.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
   const [selectedSpaceAvailable, setSelectedSpaceAvailable] = React.useState(
     ""
   );
+  const [search, setSearch] = React.useState("");
 
   let filteredData = [...Data];
   if (selectedCity) {
@@ -21,11 +22,16 @@ export default function App() {
   }
   if (selectedSpaceAvailable) {
     filteredData = filteredData.filter(
-      (data) => data.space_available === selectedSpaceAvailable
+      (data) => data.space_available == selectedSpaceAvailable
+    );
+  }
+  if (search) {
+    filteredData = filteredData.filter((data) =>
+      data.name.toLowerCase().includes(search.toLowerCase())
     );
   }
 
-  console.log(filteredData);
+  // console.log(filteredData);
   function RenderData() {
     return (
       <div>
@@ -40,6 +46,16 @@ export default function App() {
     <div className="App">
       <h1>Warehouses</h1>
       <br />
+      <div className="field">
+        <div className="control">
+          <input
+            className="input"
+            type="search"
+            placeholder="Search City"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
       <Filter
         setSelectedCity={setSelectedCity}
         setSelectedCluster={setSelectedCluster}
